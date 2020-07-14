646c1a65b09514c3ac7318694c0cd6c7b909271d
victorsc45 <victorlitzau@gmail.com>
2020-07-14 16:31:26 -0400

    new test

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# readme,
-        # create a CLI readme,
-        # none,
-        # CLI readme ,
-        # none,
-        # use node index file to create the readme from CLI
-        [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT);
+# readme
+        # readme
+        # readme
+        # readme
+        # on contributor
+        # node js CLI
+        # undefined
+         * [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,12 +1,13 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.Title},
-        # ${data.Description},
-        # ${data.Installation},
-        # ${data.Usage},
-        # ${data.Contributing},
+  return `# ${data.Title}
+        # ${data.Description}
+        # ${data.Installation}
+        # ${data.Usage}
+        # ${data.Contributing}
         # ${data.Tests}
-        [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT);
+        # ${data.License.choices}
+         * [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT);
 
 `;
 }
