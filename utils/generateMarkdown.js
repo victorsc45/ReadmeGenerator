6b8594e5d1a73f93ce261eb642a2e35e84a6872f
victorsc45 <victorlitzau@gmail.com>
2020-07-14 16:33:55 -0400

    new test

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,7 +7,7 @@ function generateMarkdown(data) {
         # ${data.Contributing}
         # ${data.Tests}
         # ${data.License.choices}
-         * [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT);
+        [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 
 `;
 }
